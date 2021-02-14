d65cddc045d2e81cdaaace79e29fbbae15dc6681
sergioa2 <s.arangoa@uniandes.edu.co>
2021-02-14 14:15:54 -0500

    import time

--- App/view.py
+++ App/view.py
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
-
+import time
 import config as cf
 import sys
 import controller
